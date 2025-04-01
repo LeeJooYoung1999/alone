5e585247fa53317d13db375d002915a31f7e15f8
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-04-01 18:10:45 +0900

    ㅇㅇ

---
+++ 기본문제+심화문제(제출용)/02 웹 인터페이스 구축을 위한 Vue.js/06_vue_이주영/06 단일 파일 컴포넌트(기본)/test-vue-app/src/App2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>관심있는 K-POP 가수?</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+    </ul>
+  </div>
+</template>
+<script>
+import CheckboxItem from './components/CheckboxItem2.vue';
+export default {
+name: 'App',
+components: {CheckboxItem},
+
+};
+</script>
