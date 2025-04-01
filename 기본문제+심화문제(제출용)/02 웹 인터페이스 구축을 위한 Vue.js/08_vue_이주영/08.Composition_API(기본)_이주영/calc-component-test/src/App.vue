850143bcdcb52acf903c79c80ff6ea15aedd3310
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-04-01 10:28:29 +0900

    08 컴포지션ap i기본문제 완료

--- 기본문제+심화문제(제출용)/02 웹 인터페이스 구축을 위한 Vue.js/08_vue_이주영/08.Composition_API(기본)_이주영/calc-component-test/src/App.vue
+++ 기본문제+심화문제(제출용)/02 웹 인터페이스 구축을 위한 Vue.js/08_vue_이주영/08.Composition_API(기본)_이주영/calc-component-test/src/App.vue
@@ -14,8 +14,11 @@
 // 슬라이드8: Calc3 컴포넌트를 사용하도록 App.vue를수정하세요.
 // import Calc from './components/Calc3.vue'; 
 
-//Calc4 컴포넌트를 다음처럼 정의하고, 필요한경우코드를완성하세요. -  버튼을 제거하고, x,y값을 입력한 경우 자동으로 결과가 계산됨.
-import Clac from './components/Calc4.vue'; 
+// 슬라이드10: Calc4 컴포넌트를 사용하도록 App.vue를수정하세요.
+// import Calc from './components/Calc4.vue'; 
+
+// 슬라이드12: Calc5 컴포넌트를사용하도록App.vue를수정하세요.
+import Calc from './components/Calc5.vue'
 export default {
   name: 'App',
   components: { Calc },
---
+++ 기본문제+심화문제(제출용)/02 웹 인터페이스 구축을 위한 Vue.js/08_vue_이주영/08.Composition_API(기본)_이주영/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,26 @@
+<!-- 슬라이드11: Calc5 컴포넌트를 다음처럼 정의하고, 필요한 경우 코드를 완성하세요
+                  -  x의 값이 변경될때마다, 이전값과 새로운 값을 브라우저 콘솔에 출력.-->
+
+<template>
+  <div>
+    x : <input type="text" v-model.number="x" />
+    <br />
+    결과 : {{ result }}
+  </div>
+</template>
+<script>
+import { ref, watch } from 'vue'; //x값의 변화를 감지해야하므로, 관찰자 옵션 watch사용 -> 임포트
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(0); // 0으로 초기화
+    const result = ref(0); // 0으로 초기화
+    // 필요한 경우 코드 추가
+    watch(x, (current, old) => {
+      console.log(`${old} ==> ${current}`);  //이전값과 새로운 값을 콘솔에 출력/ 작은따옴표' 아니고 백틱`사용해야함 주의.
+      result.value = current * 2;         //ref사용 하므로, value속성 동반
+    });
+    return { x, result };
+  },
+};
+</script>
