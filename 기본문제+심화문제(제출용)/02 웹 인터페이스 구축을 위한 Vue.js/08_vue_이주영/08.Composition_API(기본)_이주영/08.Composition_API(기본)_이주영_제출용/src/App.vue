76fbbc075be6ec78b8ec98aac341d7ba44f1c0bc
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-04-01 13:08:28 +0900

    dd

---
+++ 기본문제+심화문제(제출용)/02 웹 인터페이스 구축을 위한 Vue.js/08_vue_이주영/08.Composition_API(기본)_이주영/08.Composition_API(기본)_이주영_제출용/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+
+<script>
+// 슬라이드4: App.vue를다음과같이정의하고, Calc 컴포넌트가 올바르게 동작하는지 확인하세요. -->
+//import Calc from './components/Calc.vue';
+
+// 슬라이드6: Calc2 컴포넌트를 사용하도록 App.vue를수정하세요.
+//import Calc from './components/Calc2.vue';
+
+// 슬라이드8: Calc3 컴포넌트를 사용하도록 App.vue를수정하세요.
+// import Calc from './components/Calc3.vue'; 
+
+// 슬라이드10: Calc4 컴포넌트를 사용하도록 App.vue를수정하세요.
+// import Calc from './components/Calc4.vue'; 
+
+// 슬라이드12: Calc5 컴포넌트를사용하도록App.vue를수정하세요.
+import Calc from './components/Calc5.vue'
+export default {
+  name: 'App',
+  components: { Calc },
+};
+</script>
---
+++ 기본문제+심화문제(제출용)/02 웹 인터페이스 구축을 위한 Vue.js/08_vue_이주영/08.Composition_API(기본)_이주영/08.Composition_API(기본)_이주영_제출용/src/components/Calc4.vue
@@ -0,0 +1,23 @@
+<!-- 슬라이드9: Calc4 컴포넌트를 다음처럼 정의하고, 필요한 경우 코드를 완성하세요
+                  -  버튼을 제거하고, x,y값을 입력한 경우 자동으로 결과가 계산됨.-->
+
+<template>
+  <div>
+    X : <input type="text" v-model.number="state.x" /> <br />
+    Y : <input type="text" v-model.number="state.y" /> <br />
+    <div>결과 : {{ result }}</div>   
+  </div>
+</template>
+<script>
+ import { reactive, computed   } from 'vue';  //computed를 밑에서 연산하는데 써야하니 임포트
+ export default {
+ name: 'Calc4',
+ setup() {
+ const state = reactive({ x: 10, y: 20 });
+ const result =   computed(()=>{
+   return state.x + state.y
+ })    ;
+ return  {result, state}    ;
+},
+ };
+</script>
