494a3c60e83fbb769c2c448cf4087d13fa6d92fc
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-05-21 17:46:13 +0900

    [4주차] 감염된 폴더1 풀이시작

---
+++ Algorithm/FourthWeek/InfectedFolder/Solution.java
@@ -0,0 +1,25 @@
+package FourthWeek.InfectedFolder;
+import java.util.*;
+public class Solution {
+    public String solution (String[][]folders, String p, String q){
+        String answer = "";
+
+        return answer;
+    }//solution
+    public class treeNode {
+        String data;
+        List<treeNode> children; //List자료구조를 사용하여 자식노드목록을 저장할것.
+        public treeNode(String data) {
+            this.data = data;
+            this.children = new LinkedList<>();
+        }
+        public void addChild(treeNode child) {
+            this.children.add(child);
+        }
+        public List<treeNode> getChildren() {
+            return this.children;
+        }
+
+
+    }
+}//class
