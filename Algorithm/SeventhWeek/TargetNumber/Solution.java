79101d29b80c3f7a90d5f319ce7283cec69fdcdf
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-07-01 17:47:56 +0900

    알고리즘 7주차 타겟넘버 완료 (백트래킹 사용)

---
+++ Algorithm/SeventhWeek/TargetNumber/Solution.java
@@ -0,0 +1,20 @@
+package SeventhWeek.TargetNumber;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        int len = numbers.length;
+        boolean[] visited = new boolean[numbers.length];
+
+        return answer;
+    }
+    public void dfs(int[] numbers, int target, int len, boolean[] visited, int count){
+        if(target == 0&& len ==0){
+            return;
+        }
+        for(int i = 0; i < numbers.length; i++){
+            if
+        }
+    }
+}
