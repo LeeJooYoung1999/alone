c2c4ef31c64806a1b7833864156828d79c392e10
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-07-01 14:07:49 +0900

    알고리즘 -7주차 - 태어난김에 세계일주 문제 완료

--- Algorithm/SeventhWeek/Kian84/Solution.java
+++ Algorithm/SeventhWeek/Kian84/Solution.java
@@ -1,13 +1,23 @@
 package SeventhWeek.Kian84;
-import java.util.*;
 
 class Solution {
+    private int maxCount;
     public int solution(int balance, int[][] countries) {
-        int cnt = -1;
-
-        return cnt;
+        maxCount = 0;
+        boolean[] visited = new boolean[countries.length];
+        dfs(balance, 0, countries, visited);
+        return maxCount;
     }
-    public void isGtoGo(int balance, int[][] countries, int cnt) {
-        if(balance)
+    public void dfs(int balance, int count, int[][] countries, boolean[] visited) {
+        maxCount = Math.max(count, maxCount);
+        for (int i = 0; i < countries.length; i++) {
+            int airfair = countries[i][0];
+            int expense = countries[i][1];
+            if(!visited[i] && balance >= expense) {
+                visited[i] = true;
+                dfs(balance - airfair, count+1, countries, visited);
+                visited[i] = false;
+            }
+        }
     }
 }
